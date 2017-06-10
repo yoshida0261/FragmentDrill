9f1bedd8c8debe84998f424b9ca8dd9ad45bdf61
yoshida0261 <yoshida261@gmail.com>
2017-06-11 03:22:06 +0900

    titleフラグメントを追加

---
+++ app/src/main/java/com/starcompany/fragmentdrill/News.java
@@ -0,0 +1,19 @@
+package com.starcompany.fragmentdrill;
+
+public class News {
+
+    public static final String [] titles= {
+        "title 0",
+                "title 1",
+                "title 2",
+                "title 3"
+    };
+
+    public static final String [] Details = {
+            "Detail 0",
+            "Detail 1",
+            "Detail 2",
+            "Detail 3",
+            "Detail 4"
+    };
+}
